68783a02bea79d49922957df1944c25409beb9fa
rafaelmoreira2503 <rafaelcarvalhomoreira@gmail.com>
2016-01-24 23:41:46 -0200

    Teste email

--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -2,4 +2,18 @@ from django.test import TestCase
 class SubscribeTest(TestCase):
     def test_get(self):
         response=self.client.get('/inscricao/')
-        self.assertEqual(200,response.status.code)
+        self.assertEqual(200,response.status_code)
+
+class SubscribePostTest(TestCase):
+
+    def setUp(self):
+        data = dict(
+            name='Regis da Silva',
+            cpf='71124336656',
+            email='rafa@example.com',
+            phone='21-9999-9999',
+        )
+        self.resp= self.client.post('/inscricao/',data)
+    def test_post(self):
+
+        self.assertEqual(302,self.resp.status_code)
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,8 +1,26 @@
-from django.http import HttpResponse
+from django.core import mail
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.template.loader import render_to_string
 from eventex.subscriptions.forms import SubscriptionForm
 
 
 def subscribe(request):
-    context = {'form': SubscriptionForm()}
-    return render(request,'subscriptions/subscription_form.html', context)
+    if request.method == 'POST':
+        form = SubscriptionForm(request.POST) # vem da view
+
+        if not form.is_valid():
+
+
+            #context = dict(name='Henrique Bastos',cpf='12345678901',email='henrique@bastos.net',phone='21-9874-5367')
+
+            body = render_to_string('subscriptions/subscription_email.txt', form.cleaned_data)
+            mail.send_mail('Confirmação de Inscrição', body, 'contato@eventex.com.br', ['contato@eventex.com.br',form.cleaned_data['email']])
+
+            return HttpResponseRedirect('/inscricao/')
+        else:
+
+            return render(request,'subscriptions/subscription_form.html', form)
+    else:
+        context = {'form': SubscriptionForm()}
+        return render(request,'subscriptions/subscription_form.html', context)
